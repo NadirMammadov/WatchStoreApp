645a1741d0ad0679be48577ecb389610650ef0a7
nadir <nadir@MACHINE>
2023-03-19 15:31:10 +0400

    added PhotoStockApi

---
+++ Services/PhotoStock/PhotoStockService.API/Controllers/PhotosController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PhotoStockService.API.Dtos;
+using WastchStore.Shared.Dtos;
+using WatchStore.Shared.ControllerBase;
+
+namespace PhotoStockService.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotosController : CustomControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> PhotoSave(IFormFile photo, CancellationToken cancellationToken)
+        {
+            if (photo != null && photo.Length > 0)
+            {
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photo.FileName);
+                var stream = new FileStream(path, FileMode.Create);
+                await photo.CopyToAsync(stream, cancellationToken);
+                var returnPath = "photos/" + photo.FileName;
+                PhotoDto photoDto = new() { Url = returnPath };
+                return CreateActionResultInstance(Response<PhotoDto>.Success(photoDto, 200));
+            }
+            return CreateActionResultInstance(Response<NoContent>.Fail("photo is empty", 400));
+        }
+
+        public IActionResult PhotoDelete(string photoUrl)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photoUrl);
+            if (!System.IO.File.Exists(path))
+            {
+                return CreateActionResultInstance(Response<NoContent>.Fail("photo not found", 404));
+            }
+            System.IO.File.Delete(path);
+            return CreateActionResultInstance(Response<PhotoDto>.Success(204));
+        }
+    }
+}
---
+++ Services/PhotoStock/PhotoStockService.API/Dtos/PhotoDto.cs
@@ -0,0 +1,7 @@
+﻿namespace PhotoStockService.API.Dtos
+{
+    public class PhotoDto
+    {
+        public string Url { get; set; } = null!;
+    }
+}
